51c263a573fe368513bb43c914653267908d2d08
suengyeon <tmddus9016@google.com>
2025-07-24 20:49:06 +0900

    feat: add routing skeleton & placeholder pages including MyPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Record from './pages/Record';
+import Feed from './pages/Feed';
+import MyPage from './pages/MyPage';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/record" element={<Record />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+export default function Home() {
+    return <div>Home 페이지</div>;
+  }
+  // 파일명에 맞춰 컴포넌트 이름과 텍스트만 바꿔서 복사
+  
